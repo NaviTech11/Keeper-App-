51946e8a88465fedf64aefbb887d9e366309900d
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-08-24 09:04:28 -0500

    Add App, Header, Footr and Note Components + Some styles

---
+++ src/components/Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note(){
+
+    return (
+        <div className="note">
+            <h1>Title f Note</h1>
+            <p>content of note</p>
+        </div>
+
+    )
+}
+
+
+export default Note;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
 ReactDOM.render(
-  <h1>Hello World!</h1>  ,
-  document.getElementById('root')
+    <App />,
+    document.getElementById("root")
 );
+
+
+
 
