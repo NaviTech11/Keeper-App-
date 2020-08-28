5c25fa712ad1912d2e8bd6c134c5e86513d3b964
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-08-28 14:18:38 -0500

    Add Create Area and button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,22 @@
 import React from "react";
 import Header from "./Header";
-import Note from "./Note";
 import Footer from "./Footer";
-import notes from "../notes";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
-
-
-function App(){
-    return (
+function App() {
+  return (
     <div>
-        <Header />
-        {notes.map(note => (
-            <Note 
-                key = {note.key}
-                title = {note.title}
-                content={note.content}  
-            />
-        ))};
-        <Footer />
+      <Header />
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
+      <Footer />
     </div>
-    
-    )
+  );
 }
-
 
 export default App;
 
 
 
 
-//FUNCTION TO CREATE NOTES (REFACTORED & PASSED ANONIMOUSLY INSIDE THE MAP METHOD)
-// function createNotes(note){
-//     return (
-//     <Note 
-//         key = {note.key}
-//         title = {note.title}
-//         content={note.content}  
-//     />
-//     ) 
-// }
