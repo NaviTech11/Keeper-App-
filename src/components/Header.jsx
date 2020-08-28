952759ab03f7ceaa55547c1e830b4cd2c987f1b5
Ivan Hernandez <ivan.hdz2@outlook.com>
2020-08-28 15:59:09 -0500

    Finalized UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import ModeCommentRoundedIcon from '@material-ui/icons/ModeCommentRounded';
 
 function Header(){
     return (
     <header>
-        <h1>Keeper</h1>
+        <h1> <ModeCommentRoundedIcon /> Keeper</h1>
     </header>
     
     )
